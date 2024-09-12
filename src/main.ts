de6e9e88411763b00fe8a5146192d1f2b7eaffb0
Oleg <40015198+OlegGrushetsky@users.noreply.github.com>
2024-09-12 18:27:29 +0300

    functionality of counters

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,46 @@
-import { factory } from "./factory";
+import { factory } from "./factory/factory.ts";
 
-let count = factory(0, 1);
-
-function update_count_and_reset_counter() {}
-
-const start_at_control = document.getElementById(
-  "start_at",
+// Get elements
+const start_at_control = document.querySelector(
+  "#start_at",
 ) as HTMLInputElement;
-
 const step_control = document.getElementById("step") as HTMLInputElement;
-
-start_at_control?.addEventListener("change", () => {});
-
-step_control?.addEventListener("change", () => {});
 
 const count_button = document.querySelector(
   ".count_button",
 ) as HTMLButtonElement;
-
 const current_count = document.querySelector(
   ".current_count",
 ) as HTMLSpanElement;
 
-function update_count() {}
+// Setup listeners
+start_at_control?.addEventListener("change", () =>
+  update_count_and_reset_counter(),
+);
+step_control?.addEventListener("change", () =>
+  update_count_and_reset_counter(),
+);
 
 count_button.addEventListener("click", update_count);
+
+// Initiate counter
+let count = factory(0, 1);
+
+/**
+ * Handle counter increment
+ */
+function update_count() {
+  const value = count();
+  current_count.innerText = String(value);
+}
+
+/**
+ * Handle counter setup
+ */
+function update_count_and_reset_counter() {
+  const start = Number(start_at_control.value);
+  const step = Number(step_control.value);
+
+  count = factory(start, step);
+  current_count.innerText = String(start);
+}
